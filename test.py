273e50d25435887e949d5e41890b2c69ecceb03c
Sietze van der Vinne <sietze.poepie@gmail.com>
2025-06-27 18:26:03 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import os
 import torch.optim as optim
 import torchvision
 from backbones.ncsnpp_generator_adagn import NCSNpp
-from dataset_original import CreateDatasetSynthesis_original
+from dataset_big import CreateDatasetSynthesis
 from saved_dataset import SavedDataset
 
 from torch.utils.data import Subset
@@ -159,7 +159,7 @@ def sample_and_test(args):
 
     #loading dataset
     phase='test'
-    dataset = CreateDatasetSynthesis_original(phase, args.input_path, contrast1=args.contrast1, contrast2=args.contrast2)
+    dataset = CreateDatasetSynthesis(phase, args.input_path, contrast1=args.contrast1, contrast2=args.contrast2, num_test_samples=args.num_test_samples)
     if args.num_test_samples is not None:
         dataset = Subset(dataset, range(args.num_test_samples))
 
